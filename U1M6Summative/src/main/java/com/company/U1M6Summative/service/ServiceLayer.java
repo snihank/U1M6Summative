abb02e82faded7100824805b0becf574b6c38dbe
Shakib <nihan66@yahoo.com>
2019-08-26 13:56:10 -0400

    service layer added

---
+++ U1M6Summative/src/main/java/com/company/U1M6Summative/service/ServiceLayer.java
@@ -0,0 +1,23 @@
+package com.company.U1M6Summative.service;
+
+import com.company.U1M6Summative.dao.CustomerDao;
+import com.company.U1M6Summative.dao.InvoiceDao;
+import com.company.U1M6Summative.dao.InvoiceItemDao;
+import com.company.U1M6Summative.dao.ItemDao;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class ServiceLayer {
+
+    private CustomerDao customerDao;
+    private InvoiceDao invoiceDao;
+    private InvoiceItemDao invoiceItemDao;
+    private ItemDao itemDao;
+
+    @Autowired
+    public ServiceLayer(CustomerDao customerDao, InvoiceDao invoiceDao, InvoiceItemDao invoiceItemDao, ItemDao itemDao) {
+        this.customerDao = customerDao;
+        this.invoiceDao = invoiceDao;
+        this.invoiceItemDao = invoiceItemDao;
+        this.itemDao = itemDao;
+    }
+}
