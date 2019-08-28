b5163ae02b499ac4ec23a760ff8e9266206c6d26
SOAMI <sac593@nyu.edu>
2019-08-28 13:19:53 -0400

    YAY figured it out/dao's done

--- U1M6Summative/src/main/java/com/company/U1M6Summative/dao/InvoiceDao.java
+++ U1M6Summative/src/main/java/com/company/U1M6Summative/dao/InvoiceDao.java
@@ -1,4 +1,13 @@
 package com.company.U1M6Summative.dao;
 
+import com.company.U1M6Summative.model.Invoice;
+
+import java.util.List;
+
 public interface InvoiceDao {
+    Invoice addInvoice(Invoice invoice);
+    Invoice getInvoice(int id);
+    List<Invoice>getAllInvoices();
+    void updateInvoice(Invoice invoice);
+    void deleteInvoice(int id);
 }
--- U1M6Summative/src/main/java/com/company/U1M6Summative/dao/ItemDao.java
+++ U1M6Summative/src/main/java/com/company/U1M6Summative/dao/ItemDao.java
@@ -1,4 +1,13 @@
 package com.company.U1M6Summative.dao;
 
+import com.company.U1M6Summative.model.Item;
+
+import java.util.List;
+
 public interface ItemDao {
+    Item addItem(Item item);
+    Item getItem(int id);
+    List<Item> getAllItems();
+    void updateItem(Item item);
+    void deleteItem(int id);
 }
