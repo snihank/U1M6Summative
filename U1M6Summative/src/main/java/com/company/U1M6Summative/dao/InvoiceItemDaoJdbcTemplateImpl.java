1a14f368b8d464b93a31de1f30ff7c6f7105446e
Blora616 <Blora616@gmail.com>
2019-08-29 11:46:27 -0400

    invoice item

--- U1M6Summative/src/main/java/com/company/U1M6Summative/dao/InvoiceItemDaoJdbcTemplateImpl.java
+++ U1M6Summative/src/main/java/com/company/U1M6Summative/dao/InvoiceItemDaoJdbcTemplateImpl.java
@@ -1,10 +1,32 @@
 package com.company.U1M6Summative.dao;
 
 import com.company.U1M6Summative.model.InvoiceItem;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public class InvoiceItemDaoJdbcTemplateImpl implements InvoiceItemDao {
+
+    private JdbcTemplate jdbcTemplate;
+
+    private static final String INSERT_INVOICE_ITEM_SQL =
+            "insert into invoice (invoice_id, item_id, quantity, unit_rate, discount) values (?, ?, ?, ?, ?)";
+
+    private static final String SELECT_ALL_INVOICE_ITEMS_SQL =
+            "select * from invoice_item";
+
+    private static final String SELECT_INVOICE_SQL =
+            "select * from invoice where invoice_item_id = ?";
+
+    private static final String UPDATE_INVOICE_SQL =
+            "update invoice_item set invoice_id = ?, item_id = ?, quantity = ?, unit_rate = ?, discount= ? " +
+                    "where invoice_item_id = ?";
+
+    private static final String DELETE_INVOICE_SQL =
+            "delete from invoice where invoice_item_id = ?";
+
     @Override
     public InvoiceItem addInvoiceItem(InvoiceItem invoiceItem) {
         return null;
